c249b060a25b1117228d06c0bc9aaae4ff276dc9
masapon0618 <masapon0618@gmail.com>
2021-04-30 14:34:09 +0900

    位置情報機能を実装

--- BeaconApp/ViewController.swift
+++ BeaconApp/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+import CoreLocation
+
 class ViewController: UIViewController {
+   
+    // CoreLocation
+    var locationManager : CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        // ロケーションマネージャーオブジェクトを作成
+        self.locationManager = CLLocationManager();
+
+        // 位置情報の認証ステータスを取得
+        let status = CLLocationManager.authorizationStatus()
+       
+        // 位置情報の認証が許可されていない場合は認証ダイアログを表示
+        if (status != CLAuthorizationStatus.authorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
     }
-
-
+    
+    
 }
-
